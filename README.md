c539b64aeca7606d555dd859c0ee8f03e7c4e569
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-02-04 15:07:51 +0100

    Add a script for listing no shows.

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ A set of random scripts working on the WCA Competition Interchange Format (WCIF)
 ## Usage
 
 - `npx -q -p github:jonatanklosko/wcif-scripts generate-tasks-csv <competition-id>` - outputs task assignments in a CSV format
+- `npx -q -p github:jonatanklosko/wcif-scripts list-no-shows <competition-id>` - lists people who registered for the competition, but have no results
---
+++ src/list-no-shows.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const fetch = require('node-fetch');
+
+const flatMap = (arr, fn) => arr.reduce((xs, x) => xs.concat(fn(x)), []);
+
+const getNoShows = async (competitionId) => {
+  const response = await fetch(
+    `https://worldcubeassociation.org/api/v0/competitions/${competitionId}/wcif/public`
+  );
+  const wcif = await response.json();
+  const allResults = wcif.events
+    .flatMap(event => event.rounds)
+    .flatMap(round => round.results);
+  const participantIds = Array.from(new Set(
+    allResults.map(result => result.personId)
+  ));
+  const noShows = wcif.persons
+    .filter(person => person.registration && person.registration.status === 'accepted')
+    .filter(person => !participantIds.includes(person.registrantId));
+  return noShows;
+};
+
+const [, , competitionId] = process.argv;
+if (!competitionId) {
+  console.log('Missing argument: competition id.');
+  process.exit(1);
+}
+
+getNoShows(competitionId)
+  .then(noShows => console.log(
+    noShows.map(person => `${person.name} | ${person.wcaId || 'No WCA ID'}`).join('\n')
+  ))
+  .catch(error => console.error(`Something went wrong: ${error}`));
